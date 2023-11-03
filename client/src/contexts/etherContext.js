0adc4faee027f1de2d3f9be9d1ef4a747b7257f1
shz-code <web.shahidul.alam@gmail.com>
2023-11-03 14:29:10 +0600

    modified: methodsContext etherContext

--- client/src/contexts/etherContext.js
+++ client/src/contexts/etherContext.js
@@ -5,118 +5,118 @@ const EtherContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export const EtherContextProvider = ({ children }) => {
-    const { ethereum } = window;
+  const { ethereum } = window;
 
-    const [instance, setInstance] = useState({
-        account: "",
-        contract: "",
-        provider: null,
-        balance: null,
-    });
+  const [instance, setInstance] = useState({
+    account: null,
+    contract: null,
+    provider: null,
+    balance: null,
+  });
 
-    // Listen for metamask events
-    // accountsChanged =>
-    // chainChanged =>
-    const eventListen = (provider, account) => {
-        // Listen for accountsChanged MetaMask event
-        window.ethereum.on("accountsChanged", async () => {
-            // Check if user disconnected the wallet or changed account
-            if (window.ethereum.selectedAddress) {
-                provider = new ethers.BrowserProvider(window.ethereum);
-                let newSigner = await provider.getSigner();
-                account = await newSigner.getAddress();
-                const balance = await provider.getBalance(account);
-                setInstance({
-                    provider: provider,
-                    balance: balance,
-                    account: account,
-                });
-            } else {
-                localStorage.clear();
-                setInstance({
-                    account: "",
-                    contract: "",
-                    provider: null,
-                    balance: null,
-                });
-            }
+  // Listen for metamask events
+  // accountsChanged =>
+  // chainChanged =>
+  const eventListen = (provider, account) => {
+    // Listen for accountsChanged MetaMask event
+    window.ethereum.on("accountsChanged", async () => {
+      // Check if user disconnected the wallet or changed account
+      if (window.ethereum.selectedAddress) {
+        provider = new ethers.BrowserProvider(window.ethereum);
+        let newSigner = await provider.getSigner();
+        account = await newSigner.getAddress();
+        const balance = await provider.getBalance(account);
+        setInstance({
+          provider: provider,
+          balance: balance,
+          account: account,
         });
-
-        // Check if user changed network
-        window.ethereum.on("chainChanged", async () => {
-            let newProvider = new ethers.BrowserProvider(window.ethereum);
-            const balance = await newProvider.getBalance(account);
-            setInstance({
-                provider: newProvider,
-                balance: balance,
-                account: account,
-            });
-        });
-    };
-
-    // Disconnect frontend with MetaMask
-    const disconnect = () => {
+      } else {
         localStorage.clear();
         setInstance({
-            account: "",
-            contract: "",
-            provider: null,
-            balance: null,
+          account: null,
+          contract: null,
+          provider: null,
+          balance: null,
         });
+      }
+    });
+
+    // Check if user changed network
+    window.ethereum.on("chainChanged", async () => {
+      let newProvider = new ethers.BrowserProvider(window.ethereum);
+      const balance = await newProvider.getBalance(account);
+      setInstance({
+        provider: newProvider,
+        balance: balance,
+        account: account,
+      });
+    });
+  };
+
+  // Disconnect frontend with MetaMask
+  const disconnect = () => {
+    localStorage.clear();
+    setInstance({
+      account: null,
+      contract: null,
+      provider: null,
+      balance: null,
+    });
+  };
+
+  useEffect(() => {
+    // Check if user already gave permission to access MetaMask
+    const init = async () => {
+      if (localStorage.getItem("account")) {
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const account = await signer.getAddress();
+        const balance = await provider.getBalance(account);
+        setInstance({
+          provider: provider,
+          account: account,
+          balance: Number(balance),
+        });
+        // Listen for events
+        eventListen(provider, account);
+      }
     };
+    init();
+  }, []);
 
-    useEffect(() => {
-        // Check if user already gave permission to access MetaMask
-        const init = async () => {
-            if (localStorage.getItem("account")) {
-                const provider = new ethers.BrowserProvider(window.ethereum);
-                const signer = await provider.getSigner();
-                const account = await signer.getAddress();
-                const balance = await provider.getBalance(account);
-                setInstance({
-                    provider: provider,
-                    account: account,
-                    balance: Number(balance),
-                });
-                // Listen for events
-                eventListen(provider, account);
-            }
-        };
-        init();
-    }, []);
+  const setContract = async () => {
+    if (ethereum !== undefined) {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const account = await signer.getAddress();
+      const balance = await provider.getBalance(account);
+      setInstance({ provider: provider, account: account, balance: balance });
+      localStorage.setItem("account", account);
+      eventListen(provider, account);
+      // const contract = new ethers.Contract(
+      //   process.env.REACT_APP_CONTRACT_ADDRESS,
+      //   process.env.REACT_APP_CONTRACT_ABI,
+      //   signer
+      //   );
+      // setInstance({
+      //   account: window.ethereum.selectedAddress,
+      //   contract,
+      // });
+    } else console.log("No Ethereum Instance available");
+  };
 
-    const setContract = async () => {
-        if (ethereum !== undefined) {
-            const provider = new ethers.BrowserProvider(window.ethereum);
-            const signer = await provider.getSigner();
-            const account = await signer.getAddress();
-            const balance = await provider.getBalance(account);
-            setInstance({ provider: provider, account: account, balance: balance });
-            localStorage.setItem("account", account);
-            eventListen(provider, account);
-            // const contract = new ethers.Contract(
-            //   process.env.REACT_APP_CONTRACT_ADDRESS,
-            //   process.env.REACT_APP_CONTRACT_ABI,
-            //   signer
-            //   );
-            // setInstance({
-            //   account: window.ethereum.selectedAddress,
-            //   contract,
-            // });
-        } else console.log("No Ethereum Instance available");
-    };
-
-    return (
-        <EtherContext.Provider
-            value={{
-                instance,
-                setContract,
-                disconnect,
-            }}
-        >
-            {children}
-        </EtherContext.Provider>
-    );
+  return (
+    <EtherContext.Provider
+      value={{
+        instance,
+        setContract,
+        disconnect,
+      }}
+    >
+      {children}
+    </EtherContext.Provider>
+  );
 };
 
 export const useEtherContext = () => useContext(EtherContext);
--- client/src/contexts/methodsContext.js
+++ client/src/contexts/methodsContext.js
@@ -1,17 +1,197 @@
+import { ethers } from "ethers";
 import { createContext, useContext } from "react";
-// import { useEtherContext } from "./etherContext";
+import { useEtherContext } from "./etherContext";
 
 const MethodsContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export const MethodsContextProvider = ({ children }) => {
-    // const { instance } = useEtherContext;
+  const { instance } = useEtherContext;
 
-    // console.log(instance);
+  // Check if user is logged in
+  const checkAuth = () => {
+    if (instance.instance.contract === null) return false;
 
-    return (
-        <MethodsContext.Provider value={{}}>{children}</MethodsContext.Provider>
-    );
+    return true;
+  };
+
+  //==========================================Milestone 1======================================/
+  /**
+   *
+   * @returns Array of all communities
+   */
+  const getAllCommunities = async () => {
+    try {
+      const res = await instance.contract.getAllCommunities();
+      return res;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  /**
+   *
+   * @returns Desired Community
+   */
+  const getCommunity = async (communityId) => {
+    try {
+      const res = await instance.contract.getCommunity(communityId);
+      return res;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  /**
+   *
+   * @param userAddress
+   * @returns If the user is member of community or not
+   */
+  const checkCommunityStakeholder = async (userAddress) => {
+    if (checkAuth()) {
+      try {
+        const res = await instance.contract.checkCommunityStakeholder(
+          userAddress
+        );
+        return res;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  /**
+   *
+   * @returns ABX instance.contract native token info
+   */
+  const getABXInfo = async () => {
+    if (checkAuth()) {
+      try {
+        const res = await instance.contract.getABXInfo();
+        return res;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  /**
+   * @param ETH amount
+   * @returns How much ABX user owns
+   */
+  const buyABX = async (ETH) => {
+    if (checkAuth()) {
+      try {
+        const eth = ethers.parseEther(ETH);
+        const res = await instance.contract.buyABX({ value: eth });
+        return res;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  /**
+   * @returns How much ABX is required to create new Community
+   */
+  const getNewCommunityCost = async () => {
+    if (checkAuth()) {
+      try {
+        const res = await instance.contract.getNewCommunityCost();
+        return res;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  /**
+   * @param communityTitle new community title, @param communityDescription new community description, @param communityTokenName new community token name
+   * @param communityTokenSymbol new community token symbol @param communityInitialSupply new community native token initial @param communityTypeId Id of new community type
+   * @returns How much ABX user owns
+   */
+  const createCommunity = async (
+    communityTitle,
+    communityDescription,
+    communityTokenName,
+    communityTokenSymbol,
+    communityInitialSupply,
+    communityTypeId
+  ) => {
+    if (checkAuth()) {
+      try {
+        const res = await instance.contract.createCommunity(
+          communityTitle,
+          communityDescription,
+          communityTokenName,
+          communityTokenSymbol,
+          communityInitialSupply,
+          communityTypeId
+        );
+        return res;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  /**
+   * @param ABXAmount amount of ABX user wants to exchange
+   * @returns  New ABX and Native Token Balance
+   */
+  const exchangeABXwithCommunityNativeToken = async (
+    communityId,
+    ABXAmount
+  ) => {
+    if (checkAuth()) {
+      try {
+        const res = await instance.contract.exchangeABXwithNativeToken(
+          communityId,
+          ABXAmount
+        );
+        return res;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+  /**
+   * @param CommunityNativeToken amount of Community Native Token user wants to exchange
+   * @returns  New ABX and Native Token Balance
+   */
+  const exchangeCommunityNativeTokenWithABX = async (
+    communityId,
+    CommunityNativeToken
+  ) => {
+    if (checkAuth()) {
+      try {
+        const res = await instance.contract.exchangeCommunityNativeTokenWithABX(
+          communityId,
+          CommunityNativeToken
+        );
+        return res;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  return (
+    <MethodsContext.Provider
+      value={{
+        getAllCommunities,
+        getCommunity,
+        checkCommunityStakeholder,
+        getABXInfo,
+        buyABX,
+        getNewCommunityCost,
+        createCommunity,
+        exchangeABXwithCommunityNativeToken,
+        exchangeCommunityNativeTokenWithABX,
+      }}
+    >
+      {children}
+    </MethodsContext.Provider>
+  );
 };
 
 export const useMethodsContext = () => useContext(MethodsContext);
